b090571a866d5e9df5a4cab9fac3d8672d2ebdb4
waki285 <suzuneu@suzuneu.com>
2022-10-04 15:20:15 +0900

    wa

--- src/js/client.js
+++ src/js/client.js
@@ -1,3 +1,15 @@
-{{}
-#region 
-i
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import {} from "react-bootstrap";
+
+const App = () => {
+  return (
+    <StrictMode>
+      Hello World
+    </StrictMode>
+  );
+};
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
